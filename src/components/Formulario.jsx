6cbab0270bc26369b98b2293d218c32222b43b23
Ivan <ivanbrazas@gmail.com>
2022-09-20 11:27:25 -0300

    Hook personalizado terminaod

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import useSelectMonedas from "../hooks/useSelectMonedas";
+import { monedas } from "../data";
 
 const Formulario = () => {
+  const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
+
   return (
     <div className="formulario container min-w-[30rem] text-center">
-      <h1 className="text-3xl font-bold text-white">
+      <h1 className="text-3xl font-bold text-white leading-normal">
         Cotiza Criptomonedas al{" "}
         <span className="border-b-4 border-sky-500 py-2">Instante</span>
       </h1>
+
+      <form>
+        <SelectMonedas />
+      </form>
     </div>
   );
 };
--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const useSelectMonedas = () => {
-  return <div>useSelectMonedas</div>;
+const useSelectMonedas = (label, monedas) => {
+  const [state, setState] = useState("");
+
+  const SelectMonedas = () => (
+    <div className="px-5">
+      <label className=" block mt-14 mb-5 text-left text-2xl text-white font-bold ">
+        {label}
+      </label>
+      <select
+        onChange={(e) => setState(e.target.value)}
+        value={state}
+        className="container px-4 py-3 rounded-md"
+      >
+        {monedas.map((moneda) => {
+          return (
+            <option key={moneda.id} value={moneda.id}>
+              {moneda.nombre}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+  return [state, SelectMonedas];
 };
 
 export default useSelectMonedas;
